7001e303f670355a62230631c5d336773e3fc8ce
Mira <szewczyk.mira@gmail.com>
2020-08-02 21:58:01 +0200

    Keep host info in config

--- weekly/config.py
+++ weekly/config.py
@@ -4,3 +4,4 @@ import os
 class Config:
     SPOTIFY_CLIENT_ID = os.environ.get("SPOTIFY_CLIENT_ID")
     SPOTIFY_CLIENT_SECRET = os.environ.get("SPOTIFY_CLIENT_SECRET")
+    HOST = os.environ.get("HOST")
