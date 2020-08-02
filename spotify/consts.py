8e1b46014476460f04c81c2f4b97fa585edad0f5
Mira <szewczyk.mira@gmail.com>
2020-08-02 21:59:16 +0200

    Define spotify authentication client

---
+++ spotify/consts.py
@@ -0,0 +1,12 @@
+SPOTIFY_API_URL = "https://api.spotify.com/v1"
+
+SPOTIFY_AUTHORIZATION_URL = "https://accounts.spotify.com/authorize"
+
+SCOPE = [
+    "user-read-email",
+    "playlist-modify-public",
+    "user-library-modify",
+    "playlist-read-private",
+    "playlist-modify-private",
+    "user-library-read",
+]
