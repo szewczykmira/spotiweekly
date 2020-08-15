8f5d5d0334b52d9fdb5b7ebfff34166482b3c471
Mira <szewczyk.mira@gmail.com>
2020-08-15 21:22:36 +0200

    Add tests for spotify module

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,9 @@
 import pytest
 from weekly.app import create_app
+from weekly.config import TestConfig
 
 
 @pytest.fixture
 def app():
-    app = create_app()
+    app = create_app(TestConfig)
     return app
