55b12c72df70f873d6a7b50387ff9a766d3cfde1
Mira <szewczyk.mira@gmail.com>
2020-09-08 21:57:43 +0200

    Keep spotify token in cookie Better idea would be to keep it in session, but Safari has some issues about setting cookies in redirects

--- weekly/views.py
+++ weekly/views.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, current_app, redirect, render_template, request, session
+from flask import Blueprint, current_app, make_response, redirect, render_template, request
 from spotify.api import get_auth_url
+from spotify.consts import SPOTIFY_COOKIE_NAME
 from spotify.exceptions import CodeNotProvided
 from spotify.utils import retrieve_code
 
@@ -20,6 +21,16 @@ def authenticate():
 @weekly_bp.route("/callback")
 def callback():
     try:
-        return retrieve_code(request.args)
+        code = retrieve_code(request.args)
+        response = make_response(code)
+        response.set_cookie(SPOTIFY_COOKIE_NAME, code)
+        return response
     except CodeNotProvided:
         return "Something went wrong"
+
+
+@weekly_bp.route("/logout")
+def logout():
+    response = make_response("You have been logout")
+    response.set_cookie(SPOTIFY_COOKIE_NAME, "", max_age=0)
+    return response
