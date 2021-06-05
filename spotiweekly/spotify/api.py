064909be04ca4a78437967cfcdf4a9c81642bec2
Mira <szewczyk.mira@gmail.com>
2021-06-05 22:40:43 +0200

    Cach error in response

--- spotiweekly/spotify/api.py
+++ spotiweekly/spotify/api.py
@@ -1,7 +1,10 @@
+import json
 from dataclasses import dataclass, field
 from urllib.parse import urljoin
 
 import requests
+
+from .exceptions import InvalidResponse
 
 
 @dataclass
@@ -9,11 +12,15 @@ class SpotifyClient:
     token: str
     API_URL: str = field(init=False, default="https://api.spotify.com/v1/")
 
-    def send(self, url: str):
+    def get(self, url: str):
         url = urljoin(self.API_URL, url)
 
         headers = {"Authorization": f"Bearer {self.token}"}
-        return requests.get(url, headers=headers)
+        response = requests.get(url, headers=headers)
+        content = json.loads(response.content)
+        if "error" in content:
+            raise InvalidResponse(content["error"]["message"])
+        return content
 
     def me(self):
-        return self.send("me")
+        return self.get("me")
